1aee4f388764ee1901b65bcb722bcd2ca6b22f03
Nadil Dulnidu <nzinlud963@gmail.com>
2026-04-05 05:03:59 +0530

    init: initailize the base node.js server via docker

---
+++ server/src/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.json({
+    mode: process.env.NODE_ENV || "development",
+    message: "Express running Docker🚀",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
